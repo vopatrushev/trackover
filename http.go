4e74b738b9345dae6e6f9c57f2649b06c54ef17f
Vladimir Patrushev <patrushev@litres.ru>
2019-11-10 17:47:37 +0300

    fix

--- http.go
+++ http.go
@@ -1,19 +1,21 @@
 package main
-import {
-	"net/http"
+
+import (
 	"fmt"
-}
+	"net/http"
+)
 
 func main() {
-    http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprint(w, "About Page")
-    	})
-    http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprint(w, "Contact Page")
-    	})
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprint(w, "Index Page")
-    	})
-	 fmt.Println("Server is listening...")
-	 http.ListenAndServe("192.168.2.241:8080", msgHandler)
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "hello.html")
+	})
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About Page")
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Index Page")
+	})
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
 }
